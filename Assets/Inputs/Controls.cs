4d663a44463739af6ee6b70e0e3812aab2c31da7
Guillaume <redtri@outlook.com>
2019-11-18 16:53:32 +0100

    [ADD] 1st Striking mechanic prototype
    -Adding Sprite Editor 2D Package
    -Adding new Unity Input System Package
    - Adding PlayerController and core player gameplay logic

---
+++ Assets/Inputs/Controls.cs
@@ -0,0 +1,353 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    private InputActionAsset asset;
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Gameplay"",
+            ""id"": ""259e42ee-dad6-4613-897a-c19b37cf3599"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""f1d5674a-7328-4d37-9357-34c2f5bac584"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Button"",
+                    ""id"": ""afceca56-ccf0-4957-a51c-aac91562ed54"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Strike"",
+                    ""type"": ""Button"",
+                    ""id"": ""ac988b8e-cf08-43a1-8a1a-935f71ae9451"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Parry"",
+                    ""type"": ""Button"",
+                    ""id"": ""503aeed6-8f91-40b4-9bb5-81f1bb48f565"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""4d3b6432-8944-46e9-b956-7a48472878eb"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone"",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""ZQSD"",
+                    ""id"": ""ac92d7bf-e969-46e7-a0d8-236abb599319"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": ""NormalizeVector2"",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""06db65d6-ba4e-4170-8ae7-50fbc1d5672e"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""db7035ba-ba7e-4521-9932-8ca0abdd00bf"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""3af4f20d-cce4-4782-abc1-826175d78b09"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""ccfc3bf8-1e78-4e3e-8ef6-fd71886a8c27"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""330d3fac-9d85-4f1b-81d7-93a6b8e2ad9d"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": ""StickDeadzone"",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bfdc6049-face-4b3e-9e78-41c4aaa3a3fe"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": ""Normalize(min=-1,max=1)"",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""893d89db-644a-4717-83d0-4bbc7ee982f7"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Strike"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0783ab94-1ee1-4844-9bc6-e24b43e71d4b"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Strike"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""0392e6bf-db6a-43e1-824f-e9d523f796ee"",
+                    ""path"": ""<Gamepad>/leftTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Gamepad"",
+                    ""action"": ""Parry"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""296fa8f2-dfce-4ba7-8442-36c46fb02715"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Parry"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Keyboard&Mouse"",
+            ""bindingGroup"": ""Keyboard&Mouse"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Keyboard>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                },
+                {
+                    ""devicePath"": ""<Mouse>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        },
+        {
+            ""name"": ""Gamepad"",
+            ""bindingGroup"": ""Gamepad"",
+            ""devices"": [
+                {
+                    ""devicePath"": ""<Gamepad>"",
+                    ""isOptional"": false,
+                    ""isOR"": false
+                }
+            ]
+        }
+    ]
+}");
+        // Gameplay
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
+        m_Gameplay_Look = m_Gameplay.FindAction("Look", throwIfNotFound: true);
+        m_Gameplay_Strike = m_Gameplay.FindAction("Strike", throwIfNotFound: true);
+        m_Gameplay_Parry = m_Gameplay.FindAction("Parry", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Move;
+    private readonly InputAction m_Gameplay_Look;
+    private readonly InputAction m_Gameplay_Strike;
+    private readonly InputAction m_Gameplay_Parry;
+    public struct GameplayActions
+    {
+        private @Controls m_Wrapper;
+        public GameplayActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Gameplay_Move;
+        public InputAction @Look => m_Wrapper.m_Gameplay_Look;
+        public InputAction @Strike => m_Wrapper.m_Gameplay_Strike;
+        public InputAction @Parry => m_Wrapper.m_Gameplay_Parry;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMove;
+                @Look.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnLook;
+                @Strike.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnStrike;
+                @Strike.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnStrike;
+                @Strike.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnStrike;
+                @Parry.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnParry;
+                @Parry.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnParry;
+                @Parry.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnParry;
+            }
+            m_Wrapper.m_GameplayActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+                @Strike.started += instance.OnStrike;
+                @Strike.performed += instance.OnStrike;
+                @Strike.canceled += instance.OnStrike;
+                @Parry.started += instance.OnParry;
+                @Parry.performed += instance.OnParry;
+                @Parry.canceled += instance.OnParry;
+            }
+        }
+    }
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    private int m_KeyboardMouseSchemeIndex = -1;
+    public InputControlScheme KeyboardMouseScheme
+    {
+        get
+        {
+            if (m_KeyboardMouseSchemeIndex == -1) m_KeyboardMouseSchemeIndex = asset.FindControlSchemeIndex("Keyboard&Mouse");
+            return asset.controlSchemes[m_KeyboardMouseSchemeIndex];
+        }
+    }
+    private int m_GamepadSchemeIndex = -1;
+    public InputControlScheme GamepadScheme
+    {
+        get
+        {
+            if (m_GamepadSchemeIndex == -1) m_GamepadSchemeIndex = asset.FindControlSchemeIndex("Gamepad");
+            return asset.controlSchemes[m_GamepadSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+        void OnStrike(InputAction.CallbackContext context);
+        void OnParry(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/MonoBehaviours/PlayerController.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    //CLASSES
+    [System.Serializable]
+    public class Action {
+        public Cooldown cooldown;
+        public float actionDuration;
+
+        public bool IsActionPerforming(float time) {
+            return (time <= cooldown.lastRefreshTime + actionDuration);
+        }
+    }
+    //TODO : We may want to create a separate class file for this one as Cooldowns could be used by other classes.
+    [System.Serializable]
+    public class Cooldown {
+        public float cooldownDuration;
+        [HideInInspector] public float lastRefreshTime;
+
+        //INIT
+        public Cooldown(float tDuration, float time) {
+            cooldownDuration = tDuration;
+            lastRefreshTime = time - cooldownDuration;
+        }
+
+        public void BlankRefreshTime(float time) {
+            lastRefreshTime = time - cooldownDuration;
+        }
+
+        //TESTS
+        public bool CanRefresh(float time) {
+            return (time - lastRefreshTime > cooldownDuration);
+        }
+
+        //OPERATIONS
+        public void Refresh(float time) {
+            if (CanRefresh(time)) {
+                lastRefreshTime = time;
+            }
+        }
+    }
+    
+    //REFERENCES
+    private PlayerInput inputSystem;
+    private Animator animator;
+    private SpriteRenderer sprRenderer;
+    private Rigidbody2D rb;
+    [Header("REFERENCES")]
+    [SerializeField] private Sword sword;
+    [Header("PARAMETERS")]
+    [SerializeField] private Action strike;
+    [SerializeField] private Action parry;
+    //OTHER
+    private int playerIndex;
+    private bool facingLeft;
+    private Vector2 look;
+
+    private void Awake() {
+        inputSystem = this.GetComponent<PlayerInput>();
+        animator = this.GetComponent<Animator>();
+        rb = this.GetComponent<Rigidbody2D>();
+        sprRenderer = this.GetComponent<SpriteRenderer>();
+
+        sword.Initialize(this);
+
+        inputSystem.currentActionMap["Move"].performed += context => OnMovement(context);
+        inputSystem.currentActionMap["Move"].canceled += context => OnMovement(context);
+
+        inputSystem.currentActionMap["Look"].performed += context => OnLook(context);
+        inputSystem.currentActionMap["Look"].canceled += context => OnLook(context);
+
+        inputSystem.currentActionMap["Parry"].started += OnParry;
+        inputSystem.currentActionMap["Strike"].started += OnStrike;
+
+        strike.cooldown.BlankRefreshTime(Time.time);
+        parry.cooldown.BlankRefreshTime(Time.time);
+    }
+
+    private void Start() {
+        playerIndex = GameManager.instance.NewPlayer(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprRenderer.flipX = facingLeft;
+    }
+
+    //INPUTS
+
+    private void OnMovement(InputAction.CallbackContext value) {
+        Vector2 movement = value.ReadValue<Vector2>();
+
+        //Player cannot move on vertical axis
+        movement.y = 0;
+
+        rb.velocity = movement;
+        animator.SetFloat("speed", movement.magnitude);
+        if(movement.x < 0) {
+            facingLeft = true;
+        } else if(movement.x > 0){
+            facingLeft = false;
+        }
+    }
+
+    private void OnLook(InputAction.CallbackContext value) {
+        look = value.ReadValue<Vector2>().normalized;
+        print("Look :" + look);
+        if(look != Vector2.zero)
+            sword.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, (90f+(90*(-look.y))) * ((facingLeft) ? 1f : -1f)));
+    }
+
+    private void OnStrike(InputAction.CallbackContext value) {
+        if(strike.cooldown.CanRefresh(Time.time)) {
+            strike.cooldown.Refresh(Time.time);
+            animator.SetTrigger("strike");
+            Attack();
+        }
+    }
+
+    private void OnParry(InputAction.CallbackContext value) {
+        if (parry.cooldown.CanRefresh(Time.time)) {
+            parry.cooldown.Refresh(Time.time);
+            animator.SetTrigger("parry");
+        }
+    }
+
+    //OTHER
+
+    public bool Attack() {
+        //If the opponent sword is being overlapped
+        if (sword.overlappedSword) {
+            if (sword.overlappedSword.owner.parry.IsActionPerforming(Time.time)) {
+                //The opponent successfully parried
+                Debug.Log("Opponent parried");
+                return false;
+            } else {
+                //The opponent is being stroke
+                Debug.Log("Opponent being stroke");
+                return true;
+            }
+        }
+        Debug.Log("No opponent in range");
+        return false;
+    }
+}
